4f1234ee38e977cfffcbb6d379b4d4ba5c9ceeeb
IRISKA <143841853+1r1ska@users.noreply.github.com>
2026-01-15 16:34:13 +0300

    Update init.lua

--- bootloader/init.lua
+++ bootloader/init.lua
@@ -1,3 +1,17 @@
 -- init.lua
+-- Добавляем папку HybridOS в пути поиска модулей
+package.path = "/home/HybridOS/?.lua;/home/HybridOS/?/init.lua;" .. package.path
+package.path = "/home/HybridOS/bootloader/?.lua;" .. package.path
+package.path = "/home/HybridOS/core/?.lua;" .. package.path
+package.path = "/home/HybridOS/libs/?.lua;" .. package.path
+package.path = "/home/HybridOS/modules/?.lua;" .. package.path
+package.path = "/home/HybridOS/drivers/?.lua;" .. package.path
+package.path = "/home/HybridOS/automation/?.lua;" .. package.path
+package.path = "/home/HybridOS/gui/hybrid/?.lua;" .. package.path
+package.path = "/home/HybridOS/gui/native/?.lua;" .. package.path
+
+-- Теперь Lua сможет найти модули
 local bootstrap = require("bootloader.bootstrap")
 bootstrap.run()
+
+
